0c3b44a0bd79ffb87b9a36886d61e52a14a2b114
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-02-19 15:04:06 +0100

    Wining condition + test

--- src/main/java/TicTacToe/CheckWinningConditions.java
+++ src/main/java/TicTacToe/CheckWinningConditions.java
@@ -1,13 +1,16 @@
 package TicTacToe;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CheckWinningConditions {
 
     public String checkStatus(GameBoard gameBoard){
         Character[][] board = gameBoard.getGameBoard();
-        int winningCount = board.length > 4 ? 4 : 3;
-        if (!Arrays.asList(board).contains(" ")) return "Tie";
+        int winningCount = 3;
+        if (board.length == 5) winningCount = 4;
+        if (board.length > 5) winningCount = 5;
         //Horizontal check
         for (int i = 0; i < board.length; i++) {
             int count=1;
@@ -31,27 +34,61 @@ public class CheckWinningConditions {
             }
         }
         //Diagonal check 1
-        for (int i = 0; i < board.length-1; i++) {
+        for (int shift = 0; shift < board.length-1; shift++) {
             int count=1;
-            for (int j = 1; j < board[i].length-1; j++) {
-                if ((board[i][j-1]==board[i+j][j]) && (board[i][j-1] != MoveList.EMPTY.getValue())) {
+            for (int i = 0; i < board[shift].length-1-shift; i++) {
+                if ((board[i][i+shift]==board[i+1][i+shift+1]) && (board[i][i+shift]!= MoveList.EMPTY.getValue())) {
                     count++;
-                    if (count >= winningCount) return board[i][j].toString();
+                    if (count >= winningCount) return board[i][i+shift].toString();
+                }
+                else count =1;
+            }
+        }
+        for (int shift = 0; shift < board.length-1; shift++) {
+            int count=1;
+            for (int i = 0; i < board[shift].length-1-shift; i++) {
+                if ((board[i+shift][i]==board[i+shift+1][i+1]) && (board[i+shift][i]!= MoveList.EMPTY.getValue())) {
+                    count++;
+                    if (count >= winningCount) return board[i][i+shift].toString();
                 }
                 else count =1;
             }
         }
         //Diagonal check 2
-        for (int i = 0; i < board.length-1; i++) {
+        for (int shift = 0; shift < board.length-1; shift++) {
             int count=1;
-            for (int j = 1; j < board[i].length-1; j++) {
-                if ((board[j-1][i]==board[j][i+j]) && (board[j-1][i] != MoveList.EMPTY.getValue())) {
+            for (int i = 0; i < board[shift].length-1-shift; i++) {
+                if ((board[i][(board.length-1)-(i+shift)]==board[i+1][(board.length-1)-(i+shift+1)])
+                        && (board[i][(board.length-1)-(i+shift)]!= MoveList.EMPTY.getValue())) {
                     count++;
-                    if (count >= winningCount) return board[j-1][i].toString();
+                    if (count >= winningCount) return board[i][i+shift].toString();
                 }
                 else count =1;
             }
         }
+        for (int shift = 0; shift < board.length-1; shift++) {
+            int count=1;
+            for (int i = 0; i < board[shift].length-1-shift; i++) {
+                if ((board[(board.length-1)-(i+shift)][i]==board[(board.length-1)-(i+shift+1)][i+1])
+                        && (board[(board.length-1)-(i+shift)][i]!= MoveList.EMPTY.getValue())) {
+                    count++;
+                    if (count >= winningCount) return board[i][i+shift].toString();
+                }
+                else count =1;
+            }
+        }
+
+        //Tie
+        boolean tie = true;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 1; j < board[i].length; j++) {
+                if ((board[i][j] == MoveList.EMPTY.getValue())) {
+                    tie = false;
+                    break;
+                }
+            }
+        }
+        if (tie) return "Tie";
         return "Continue";
     }
 }
--- src/test/java/TicTacToeTestSuite.java
+++ src/test/java/TicTacToeTestSuite.java
@@ -1,4 +1,6 @@
 import TicTacToe.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
@@ -10,112 +12,209 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TicTacToeTestSuite {
-    @Test
-    void testEmptyTerminalDisplay(){
-        //Given
-        GameBoard gameBoard = new GameBoard(5);
-        TerminalDisplay terminalDisplay = new TerminalDisplay();
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        //When
-        terminalDisplay.Display(gameBoard);
-        //Then
-        assertEquals("""                    
-                | | | | | |
-                | | | | | |
-                | | | | | |
-                | | | | | |
-                | | | | | |""", outContent.toString().trim());
-    }
-    @Test
-    void testTerminalDisplay()
-    {
-        //Given
-        GameBoard gameBoard = new GameBoard(5);
-        char a = 'a';
-        for (int i = 0; i < gameBoard.getGameBoard().length ; i++) {
-            for (int j = 0; j < gameBoard.getGameBoard()[i].length; j++) {
-                gameBoard.setGameBoardItem(i,j,a);
-                a++;
-            }
+    @Nested
+    @DisplayName("Terminal tests")
+    class terminal {
+        @Test
+        void testEmptyTerminalDisplay() {
+            //Given
+            GameBoard gameBoard = new GameBoard(5);
+            TerminalDisplay terminalDisplay = new TerminalDisplay();
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outContent));
+            //When
+            terminalDisplay.Display(gameBoard);
+            //Then
+            assertEquals("""                    
+                    | | | | | |
+                    | | | | | |
+                    | | | | | |
+                    | | | | | |
+                    | | | | | |""", outContent.toString().trim());
         }
-        TerminalDisplay terminalDisplay = new TerminalDisplay();
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        //When
-        terminalDisplay.Display(gameBoard);
-        //Then
-        assertEquals("""
-                |a|b|c|d|e|
-                |f|g|h|i|j|
-                |k|l|m|n|o|
-                |p|q|r|s|t|
-                |u|v|w|x|y|""", outContent.toString().trim());
+
+        @Test
+        void testTerminalDisplay() {
+            //Given
+            GameBoard gameBoard = new GameBoard(5);
+            char a = 'a';
+            for (int i = 0; i < gameBoard.getGameBoard().length; i++) {
+                for (int j = 0; j < gameBoard.getGameBoard()[i].length; j++) {
+                    gameBoard.setGameBoardItem(i, j, a);
+                    a++;
+                }
+            }
+            TerminalDisplay terminalDisplay = new TerminalDisplay();
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outContent));
+            //When
+            terminalDisplay.Display(gameBoard);
+            //Then
+            assertEquals("""
+                    |a|b|c|d|e|
+                    |f|g|h|i|j|
+                    |k|l|m|n|o|
+                    |p|q|r|s|t|
+                    |u|v|w|x|y|""", outContent.toString().trim());
+        }
+
+        @Test
+        void testPlayerNameInput() {
+            //Given
+            String input = "Konrad";
+            InputStream in = new ByteArrayInputStream(input.getBytes());
+            System.setIn(in);
+            TerminalInput terminalInput = new TerminalInput();
+            //When
+            String scannedName = terminalInput.scanPlayerName();
+            //Then
+            assertEquals(input, scannedName);
+        }
+
+        @Test
+        void testBoardSizeInput() {
+            //Given
+            String input = "4\n";
+            InputStream in = new ByteArrayInputStream(input.getBytes());
+            System.setIn(in);
+            TerminalInput terminalInput = new TerminalInput();
+            //When
+            int scannedValue = terminalInput.scanBoardSize();
+            //Then
+            assertEquals(4, scannedValue);
+        }
+
+        @Test
+        void testBoardSizeInputMismatch() {
+            //Given
+            String input = "Not number";
+            InputStream in = new ByteArrayInputStream(input.getBytes());
+            System.setIn(in);
+            TerminalInput terminalInput = new TerminalInput();
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outContent));
+            //When
+            terminalInput.scanBoardSize();
+            //Then
+            assertEquals("Please enter board size(Values less than 3 will default to 3): \n" +
+                    "Please enter a number!", outContent.toString().trim());
+        }
     }
-    @Test
-    void testPlayerNameInput(){
-        //Given
-        String input = "Konrad";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        TerminalInput terminalInput = new TerminalInput();
-        //When
-        String scannedName = terminalInput.scanPlayerName();
-        //Then
-        assertEquals(input, scannedName);
+    @Nested
+    @DisplayName("Logic tests")
+    class logic {
+        @Test
+        void testPlayer() {
+            //Given & When
+            Player player = new Player("Konrad", false);
+            Player player1 = new Player("AI", true);
+            //Then
+            assertEquals("Konrad", player.getName());
+            assertFalse(player.isAI());
+            assertEquals("AI", player1.getName());
+            assertTrue(player1.isAI());
+        }
+
+        @Test
+        void testPlayerList() {
+            //Given
+            Player player = new Player("Konrad", false);
+            Player player1 = new Player("AI", true);
+            PlayerList playerList = new PlayerList();
+            //When
+            playerList.addPlayer(player);
+            playerList.addPlayer(player1);
+            //Then
+            assertEquals(0, playerList.getPlayerNumber(player));
+            assertEquals(1, playerList.getPlayerNumber(player1));
+            assertEquals(player, playerList.getPlayer(0));
+            assertEquals(player1, playerList.getPlayer(1));
+        }
     }
-    @Test
-    void testBoardSizeInput(){
-        //Given
-        String input = "4\n";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        TerminalInput terminalInput = new TerminalInput();
-        //When
-        int scannedValue = terminalInput.scanBoardSize();
-        //Then
-        assertEquals(4, scannedValue);
-    }
-    @Test
-    void testBoardSizeInputMismatch(){
-        //Given
-        String input = "Not number";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        TerminalInput terminalInput = new TerminalInput();
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        //When
-        terminalInput.scanBoardSize();
-        //Then
-        assertEquals("Please enter board size(Values less than 3 will default to 3): \n" +
-                "Please enter a number!", outContent.toString().trim());
-    }
-    @Test
-    void testPlayer(){
-        //Given & When
-        Player player = new Player("Konrad", false);
-        Player player1 = new Player("AI", true);
-        //Then
-        assertEquals("Konrad", player.getName());
-        assertFalse(player.isAI());
-        assertEquals("AI", player1.getName());
-        assertTrue(player1.isAI());
-    }
-    @Test
-    void testPlayerList(){
-        //Given
-        Player player = new Player("Konrad", false);
-        Player player1 = new Player("AI", true);
-        PlayerList playerList = new PlayerList();
-        //When
-        playerList.addPlayer(player);
-        playerList.addPlayer(player1);
-        //Then
-        assertEquals(0,playerList.getPlayerNumber(player));
-        assertEquals(1,playerList.getPlayerNumber(player1));
-        assertEquals(player, playerList.getPlayer(0));
-        assertEquals(player1, playerList.getPlayer(1));
+    @Nested
+    @DisplayName("Win condition tests")
+    class WinConditions{
+        @Test
+        void tie(){
+            //Given
+            GameBoard gameBoard = new GameBoard(3);
+            CheckWinningConditions winConditions = new CheckWinningConditions();
+            gameBoard.setGameBoardItem(0,0,MoveList.X.getValue());
+            gameBoard.setGameBoardItem(0,1,MoveList.X.getValue());
+            gameBoard.setGameBoardItem(0,2,MoveList.O.getValue());
+            gameBoard.setGameBoardItem(1,0,MoveList.O.getValue());
+            gameBoard.setGameBoardItem(1,1,MoveList.O.getValue());
+            gameBoard.setGameBoardItem(1,2,MoveList.X.getValue());
+            gameBoard.setGameBoardItem(2,0,MoveList.X.getValue());
+            gameBoard.setGameBoardItem(2,1,MoveList.O.getValue());
+            gameBoard.setGameBoardItem(2,2,MoveList.O.getValue());
+//            TerminalDisplay terminalDisplay = new TerminalDisplay();
+//            terminalDisplay.Display(gameBoard);
+            //When
+            String result = winConditions.checkStatus(gameBoard);
+            //Then
+            assertEquals("Tie", result);
+        }
+        @Test
+        void horizontal(){
+            //Given
+            GameBoard gameBoard = new GameBoard(3);
+            CheckWinningConditions winConditions = new CheckWinningConditions();
+            gameBoard.setGameBoardItem(0,0,MoveList.X.getValue());
+            gameBoard.setGameBoardItem(0,1,MoveList.X.getValue());
+            gameBoard.setGameBoardItem(0,2,MoveList.X.getValue());
+//            TerminalDisplay terminalDisplay = new TerminalDisplay();
+//            terminalDisplay.Display(gameBoard);
+            //When
+            String result = winConditions.checkStatus(gameBoard);
+            //Then
+            assertEquals(MoveList.X.getValue().toString(), result);
+        }
+        @Test
+        void vertical(){
+            //Given
+            GameBoard gameBoard = new GameBoard(3);
+            CheckWinningConditions winConditions = new CheckWinningConditions();
+            gameBoard.setGameBoardItem(0,0,MoveList.X.getValue());
+            gameBoard.setGameBoardItem(1,0,MoveList.X.getValue());
+            gameBoard.setGameBoardItem(2,0,MoveList.X.getValue());
+//            TerminalDisplay terminalDisplay = new TerminalDisplay();
+//            terminalDisplay.Display(gameBoard);
+            //When
+            String result = winConditions.checkStatus(gameBoard);
+            //Then
+            assertEquals(MoveList.X.getValue().toString(), result);
+        }
+        @Test
+        void diagonal(){
+            //Given
+            GameBoard gameBoard = new GameBoard(3);
+            CheckWinningConditions winConditions = new CheckWinningConditions();
+            gameBoard.setGameBoardItem(0,0,MoveList.X.getValue());
+            gameBoard.setGameBoardItem(1,1,MoveList.X.getValue());
+            gameBoard.setGameBoardItem(2,2,MoveList.X.getValue());
+//            TerminalDisplay terminalDisplay = new TerminalDisplay();
+//            terminalDisplay.Display(gameBoard);
+            //When
+            String result = winConditions.checkStatus(gameBoard);
+            //Then
+            assertEquals(MoveList.X.getValue().toString(), result);
+        }
+        @Test
+        void diagonal2(){
+            //Given
+            GameBoard gameBoard = new GameBoard(3);
+            CheckWinningConditions winConditions = new CheckWinningConditions();
+            gameBoard.setGameBoardItem(0,2,MoveList.X.getValue());
+            gameBoard.setGameBoardItem(1,1,MoveList.X.getValue());
+            gameBoard.setGameBoardItem(2,0,MoveList.X.getValue());
+//            TerminalDisplay terminalDisplay = new TerminalDisplay();
+//            terminalDisplay.Display(gameBoard);
+            //When
+            String result = winConditions.checkStatus(gameBoard);
+            //Then
+            assertEquals(MoveList.X.getValue().toString(), result);
+        }
     }
 
 }
